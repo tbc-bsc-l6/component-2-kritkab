88172d8de5bf9ff6f7182368186b9ec4c7f44e13
98412356kri@gmail.com <107605732+kritikaa33@users.noreply.github.com>
2025-01-20 19:08:39 +0545

    book storer added

--- bookreviewproject/routes/web.php
+++ bookreviewproject/routes/web.php
@@ -27,6 +27,7 @@ Route::group(['prefix' => 'account'], function(){
         Route::post('update-profile', [AccountController::class,'updateProfile']) ->name ('account.updateProfile');
         Route::get('books', [BookController::class,'index']) ->name ('books.index');
         Route::get('books/create', [BookController::class,'create']) ->name ('books.create');
+        Route::post('books', [BookController::class,'store']) ->name ('books.store');
 
 
     });
